53e9cb9885245d67663c502ce9751e830c169efb
Trần Công Hùng <121417741+TranCongHung2512@users.noreply.github.com>
2023-04-06 12:37:03 +0700

    Update dangki.php

--- dangki.php
+++ dangki.php
@@ -8,37 +8,36 @@ $mobile = $_POST['mobile'];
 
 
 // check data
-$query = 'SELECT * FROM `user` WHERE `email` = "'.$email.'"';
-$data = mysqli_query($conn, $query);
-$numrow = mysqli_num_rows($data);
+ $query = 'SELECT * FROM `user` WHERE `email` = "'.$email.'"';
+ $data = mysqli_query($conn, $query);
+ $numrow = mysqli_num_rows($data);
 
 
-if ($numrow > 0){
-    $arr = [
-		'success' => false,
-		'message' => "Email da ton tai"
-	];
+ if ($numrow > 0){
+     $arr = [
+ 		'success' => false,
+ 		'message' => "Email da ton tai"
+ 	];
 
-}else{
+ }else{
     //insert
-        $query = 'INSERT INTO `user`(`email`, `pass`, `username`, `mobile`) VALUES ("'.$email.'","'.$pass.'","'.$username.'","'.$mobile.'", )';
+        $query = 'INSERT INTO `user`(`email`, `pass`, `username`, `mobile`) VALUES ("'.$email.'","'.$pass.'","'.$username.'","'.$mobile.'")';
         $data = mysqli_query($conn, $query);
 
-        if ($data == true) {
-	        $arr = [
+         if ($data == true) {
+ 	        $arr = [
 		        'success' => true,
                 'message' => "thanh cong"
             
-	                ];
-        }else{
-            $arr = [
-                'success' => false,
-                'message' => " khong thanh cong"
+ 	                ];
+         }else{
+             $arr = [
+                 'success' => false,
+                 'message' => " khong thanh cong"
                 
-	                ];
-        }
-    }
+ 	                ];
+         }
+     }
 
-print_r(json_encode($arr));
-
-?>
+ print_r(json_encode($arr));
+// ?>
